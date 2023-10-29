dd6b96d13b0cee5e1e408a2b7df8e2da519080e5
Cjay Landero <xjaylandero@gmail.com>
2023-10-29 19:12:21 +0800

    Tweak some changes in MMMR

--- Module 5/MeanMedianMode/MeanMedianMode.java
+++ Module 5/MeanMedianMode/MeanMedianMode.java
@@ -1,15 +1,66 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MeanMedianMode {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        System.out.println("Given array: " + Arrays.toString(arr));
-        System.out.println("Mean: " + calculateMean(arr));
-        System.out.println("Median: " + calculateMedian(arr));
-        System.out.println("Mode: " + calculateMode(arr));
-        System.out.println("Range: " + calculateRange(arr));
+        int[] arr1 = { 4, 9, 1, 0, 4 };
+        int[] arr2 = { 1, 7, 3, 9, 1, 2, 7 };
+        int[] arr3 = { 9, 2, 2, 4, 6, 6, 8, 8 };
+        int[] arr4 = { 3, 3, 2, 2, 1, 1, 6, 6 };
+        int[] arr5 = { 3, 3, 3, 3, 3, 3 };
+        int[] arr6 = { 1, 8, 0, 7, 1, 2, 11 };
+        int[] arr7 = { 2, 5, 1, 5, 7, 2, 1, 3 };
+
+        System.out.println("Given array: " + Arrays.toString(arr1));
+        System.out.println("Mean: " + calculateMean(arr1));
+        System.out.println("Median: " + calculateMedian(arr1));
+        System.out.println("Mode: " + calculateMode(arr1));
+        System.out.println("Range: " + calculateRange(arr1));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr2));
+        System.out.println("Mean: " + calculateMean(arr2));
+        System.out.println("Median: " + calculateMedian(arr2));
+        System.out.println("Mode: " + calculateMode(arr2));
+        System.out.println("Range: " + calculateRange(arr2));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr3));
+        System.out.println("Mean: " + calculateMean(arr3));
+        System.out.println("Median: " + calculateMedian(arr3));
+        System.out.println("Mode: " + calculateMode(arr3));
+        System.out.println("Range: " + calculateRange(arr3));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr4));
+        System.out.println("Mean: " + calculateMean(arr4));
+        System.out.println("Median: " + calculateMedian(arr4));
+        System.out.println("Mode: " + calculateMode(arr4));
+        System.out.println("Range: " + calculateRange(arr4));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr5));
+        System.out.println("Mean: " + calculateMean(arr5));
+        System.out.println("Median: " + calculateMedian(arr5));
+        System.out.println("Mode: " + calculateMode(arr5));
+        System.out.println("Range: " + calculateRange(arr5));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr6));
+        System.out.println("Mean: " + calculateMean(arr6));
+        System.out.println("Median: " + calculateMedian(arr6));
+        System.out.println("Mode: " + calculateMode(arr6));
+        System.out.println("Range: " + calculateRange(arr6));
+        System.out.println();
+
+        System.out.println("Given array: " + Arrays.toString(arr7));
+        System.out.println("Mean: " + calculateMean(arr7));
+        System.out.println("Median: " + calculateMedian(arr7));
+        System.out.println("Mode: " + calculateMode(arr7));
+        System.out.println("Range: " + calculateRange(arr7));
+        System.out.println();
     }
 
     public static double calculateMean(int[] arr) {
@@ -30,20 +81,32 @@ public class MeanMedianMode {
         }
     }
 
-    public static int calculateMode(int[] arr) {
+    public static ArrayList<Integer> calculateMode(int[] arr) {
         Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : arr) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
         int maxFreq = 0;
-        int mode = 0;
+        ArrayList<Integer> mode = new ArrayList<>();
+
         for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
             int freq = entry.getValue();
             if (freq > maxFreq) {
                 maxFreq = freq;
-                mode = entry.getKey();
             }
         }
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            int freq = entry.getValue();
+            if (freq == maxFreq) {
+                mode.add(entry.getKey());
+            }
+        }
+
+        if (mode.size() == arr.length) {
+            mode.clear();
+        }
+
         return mode;
     }
 
